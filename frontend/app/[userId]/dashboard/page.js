24eddbee6b27bead57b0c0bd29da92bcb6442f1a
Natalia Lapina <lapinann08@gmail.com>
2024-11-09 20:15:22 +0100

    fix: add new components in Dashboard

--- frontend/app/[userId]/dashboard/page.js
+++ frontend/app/[userId]/dashboard/page.js
@@ -5,27 +5,15 @@ import ControlPanel from "./ControlPanel";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
-
-function CustomTabPanel({ children, value, index, ...other }) {
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ pt: 3 }}>{children}</Box>}
-    </div>
-  );
-}
+import ApplicationBoard from "./ApplicationBoard";
+import TabPanel from "./TabPanel";
 
 export default function DashboardPage({ params }) {
   const userId = params.userId;
-  const [tabValue, setTabValue] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
 
   const handleTabChange = (event, newValue) => {
-    setTabValue(newValue);
+    setActiveTab(newValue);
   };
 
   return (
@@ -44,26 +32,13 @@ export default function DashboardPage({ params }) {
             smoothly — all in one place.
           </Typography>
         </Box>
-        <ControlPanel tabValue={tabValue} onTabChange={handleTabChange} />
+        <ControlPanel tabValue={activeTab} onTabChange={handleTabChange} />
 
-        {/* Main Content */}
-        <CustomTabPanel value={tabValue} index={0}>
-          <Box>
-            <Typography variant='h6' sx={{ mb: 2 }}>
-              In Progress Applications
-            </Typography>
-            {/* Add your "In Progress" content here */}
-          </Box>
-        </CustomTabPanel>
-
-        <CustomTabPanel value={tabValue} index={1}>
-          <Box>
-            <Typography variant='h6' sx={{ mb: 2 }}>
-              Completed Applications
-            </Typography>
-            {/* Add your "Done" content here */}
-          </Box>
-        </CustomTabPanel>
+        {[true, false].map((isActive, index) => (
+          <TabPanel key={`tab-${index}`} value={activeTab} index={index}>
+            <ApplicationBoard isActive={isActive} />
+          </TabPanel>
+        ))}
       </Container>
     </Box>
   );
