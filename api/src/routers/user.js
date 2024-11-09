48f3ef3d5d2f28de574a9471d02d15c53466a426
FurkannOzbek <furkannozbek@yandex.com>
2024-11-09 13:41:07 +0100

    Changed endpoint export name to be fit with rest of the api endpoints

--- api/src/routers/user.js
+++ api/src/routers/user.js
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import knex from "../database_client.js";
 
-const userRoute = express.Router();
+const user = express.Router();
 // JWT's secret Key
 const SECRET_KEY = process.env.JWT_SECRET;
 
@@ -10,7 +10,6 @@ const SECRET_KEY = process.env.JWT_SECRET;
 function authenticateToken(req, res, next) {
   const tokenAuth = req.header("Authorization")?.split(" ")[1];
   if (!tokenAuth) return res.status(401).json({ message: "Unauthorized" });
-
   jwt.verify(tokenAuth, SECRET_KEY, (err, userInfo) => {
     if (err) return res.status(403).json({ message: "Forbidden" });
     req.userInfo = userInfo;
@@ -18,13 +17,12 @@ function authenticateToken(req, res, next) {
   });
 }
 // The endpoint is for getting user information in a safe way
-userRoute.get("/me", authenticateToken, async (req, res) => {
+user.get("/me", authenticateToken, async (req, res) => {
   const requestedId = req.userInfo.userId;
   const userData = await knex("user").where({ user_id: requestedId }).first();
   //if user not found
   if (!userData) return res.status(404).json({ message: "User not found" });
-  // If user is found and authenticated then returning the user data
   return res.status(200).json({ userData });
 });
 
-export default userRoute;
+export default user;
