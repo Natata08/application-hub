de8430f21701723e93ba8c49b8b87ece74391931
FurkannOzbek <furkannozbek@yandex.com>
2024-11-10 01:02:49 +0100

    In response to the frontend request, the response format has been changed to JSON

--- api/src/routers/register.js
+++ api/src/routers/register.js
@@ -8,13 +8,13 @@ const register = express.Router();
 register.post("/", async (req, res) => {
   const { email, password, first_name, last_name } = req.body;
   if (!email || !password || !first_name || !last_name) {
-    return res.status(400).send("all fields required");
+    return res.status(400).json({ message: "All fields are required" });
   }
   try {
     // Checking if the username already exists
     const existedUser = await knex("user").where({ email }).first();
     if (existedUser) {
-      return res.status(400).send("Username already exists");
+      return res.status(400).json({ message: "Email already in use" });
     }
     // Hashing the password before save in dB
     const saltRounds = 10;
@@ -25,10 +25,9 @@ register.post("/", async (req, res) => {
       last_name: last_name,
       password_hash: hashedPassword,
     });
-    res.status(201).send("registered succesfully");
+    res.status(201).json({ message: "Registration was successful" });
   } catch (error) {
-    console.log("Error registering the user");
-    res.status(500).send("Error registrariton: " + error.message);
+    res.status(500).json({ error: `Registration error: ${error.message}` });
   }
 });
 
