a5d2fe938954cf2c3c9b36fb68f1bf31bbc6f1b9
Pankaj Kumar Singh <psingh@seatgeek.com>
2024-11-10 14:20:43 +0100

    feat: integrate Husky for pre-commit hooks and revise scripts
    - Add Husky to both API and frontend projects for enforcing code quality rules
    - Update scripts in package.json files:
       - Include 'prepare' script for Husky installation
       - Rename 'check' script to 'lint' for better semantics
       - Add 'format' script in frontend for code formatting using Prettier
    - Rearrange peerDependencies in frontend package.json for readability

--- api/src/routers/register.js
+++ api/src/routers/register.js
@@ -1,34 +1,34 @@
-import express from "express";
-import knex from "../database_client.js";
-import bcrypt from "bcrypt";
+import express from 'express'
+import knex from '../database_client.js'
+import bcrypt from 'bcrypt'
 
-const register = express.Router();
+const register = express.Router()
 
 // Registration
-register.post("/", async (req, res) => {
-  const { email, password, first_name, last_name } = req.body;
+register.post('/', async (req, res) => {
+  const { email, password, first_name, last_name } = req.body
   if (!email || !password || !first_name || !last_name) {
-    return res.status(400).json({ message: "All fields are required" });
+    return res.status(400).json({ message: 'All fields are required' })
   }
   try {
     // Checking if the username already exists
-    const existedUser = await knex("user").where({ email }).first();
+    const existedUser = await knex('user').where({ email }).first()
     if (existedUser) {
-      return res.status(400).json({ message: "Email already in use" });
+      return res.status(400).json({ message: 'Email already in use' })
     }
     // Hashing the password before save in dB
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    await knex("user").insert({
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+    await knex('user').insert({
       email: email,
       first_name: first_name,
       last_name: last_name,
       password_hash: hashedPassword,
-    });
-    res.status(201).json({ message: "Registration was successful" });
+    })
+    res.status(201).json({ message: 'Registration was successful' })
   } catch (error) {
-    res.status(500).json({ error: `Registration error: ${error.message}` });
+    res.status(500).json({ error: `Registration error: ${error.message}` })
   }
-});
+})
 
-export default register;
+export default register
--- frontend/components/fetches/useLogIn.js
+++ frontend/components/fetches/useLogIn.js
@@ -1,9 +1,12 @@
+import { KeyboardDoubleArrowDown } from "@mui/icons-material";
+
 export const useLogIn = async (contactData) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+         "Content-Type": 'application/json',
     },
+
     body: JSON.stringify(contactData),
   });
 
