c3ecc1c76046e52f19829309e71dc1418d95e0b4
Natalia Lapina <lapinann08@gmail.com>
2024-11-12 15:32:02 +0100

    fix: add function to handle recieiving data from LS

--- frontend/utils/localStorage.js
+++ frontend/utils/localStorage.js
@@ -1,11 +1,16 @@
 // this is to handle server-side rendering correctly
 export const getLocalStorageItem = (key) => {
-  // check if we're in a browser environment
+  // Check if it is browser environment
   if (typeof window === 'undefined') return null
 
   try {
     const item = localStorage.getItem(key)
-    return item ? JSON.parse(item) : null
+    // Attempt to parse the item as JSON, if it fails -> return as it is
+    try {
+      return JSON.parse(item)
+    } catch {
+      return item
+    }
   } catch (error) {
     console.error(`Error reading ${key} from localStorage:`, error)
     return null
