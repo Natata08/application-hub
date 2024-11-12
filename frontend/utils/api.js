8f93ccdc50de85863bbea07559ef0b47cb62a964
Natalia Lapina <lapinann08@gmail.com>
2024-11-12 15:33:14 +0100

    feat: add function to fetch applications

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,3 +1,7 @@
+import { getLocalStorageItem } from './localStorage'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
 export const fetchQuote = async (setQuote, setIsLoading, setError) => {
   try {
     setIsLoading(true)
@@ -17,6 +21,42 @@ export const fetchQuote = async (setQuote, setIsLoading, setError) => {
 
     const data = await response.json()
     setQuote(data[0])
+  } catch (err) {
+    setError(err.message)
+  } finally {
+    setIsLoading(false)
+  }
+}
+
+export const fetchApplications = async (
+  setApplications,
+  setIsLoading,
+  setError
+) => {
+  try {
+    const authToken = getLocalStorageItem('authToken')
+
+    if (!authToken) {
+      throw new Error('Authentication token not found')
+    }
+
+    setIsLoading(true)
+    setError(null)
+    const response = await fetch(`${API_URL}/user/applications`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch applications')
+    }
+
+    const data = await response.json()
+    setApplications(data)
   } catch (err) {
     setError(err.message)
   } finally {
