edbe9e4567c90673b45bb75fede1d0cb1725da65
FurkannOzbek <furkannozbek@yandex.com>
2024-11-14 12:30:54 +0100

    fix: added back console.error

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -24,6 +24,7 @@ export const getUserApplications = async (req, res) => {
 
     res.json(applications)
   } catch (error) {
+    console.error(error)
     res.status(500).json({ error: 'Error fetching applications' })
   }
 }
