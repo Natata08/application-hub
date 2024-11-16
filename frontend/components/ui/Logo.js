73b36eeac166e9486075c517eb4338ddd21d3c13
myselfankitaa <ankitaa.mishra@gmail.com>
2024-11-16 16:38:25 +0100

    Removed white space around the Logo

--- frontend/components/ui/Logo.js
+++ frontend/components/ui/Logo.js
@@ -1,30 +1,35 @@
 import Image from 'next/image'
 import { useThemeContext } from '@/components/styles/ThemeApp'
+import Link from 'next/link'
 
 export default function Logo({ width = 100, height = 100 }) {
   const { isLightMode } = useThemeContext()
   return (
     <>
       {isLightMode ? (
-        <Image
-          src="/fona.png"
-          alt="Logo"
-          width={width}
-          height={height}
-          priority
-          sizes="(max-width: 600px) 60px, (max-width: 960px) 80px, 100px"
-          style={{ width: 'auto', height: 'auto' }}
-        />
+        <Link href={'/'} passHref>
+          <Image
+            src="/fona.png"
+            alt="Logo"
+            width={width}
+            height={height}
+            priority
+            sizes="(max-width: 600px) 60px, (max-width: 960px) 80px, 100px"
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </Link>
       ) : (
-        <Image
-          src="/fona-orange.png"
-          alt="Logo"
-          width={width}
-          height={height}
-          priority
-          sizes="(max-width: 600px) 60px, (max-width: 960px) 80px, 100px"
-          style={{ width: 'auto', height: 'auto' }}
-        />
+        <Link href={'/'} passHref>
+          <Image
+            src="/fona-orange.png"
+            alt="Logo"
+            width={width}
+            height={height}
+            priority
+            sizes="(max-width: 600px) 60px, (max-width: 960px) 80px, 100px"
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </Link>
       )}
     </>
   )
