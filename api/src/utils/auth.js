218357fe214b5f44dead53e07be3fd07ee759fd8
myselfankitaa <ankitaa.mishra@gmail.com>
2024-11-18 14:01:56 +0100

    install uuid package

--- api/src/utils/auth.js
+++ api/src/utils/auth.js
@@ -1,7 +1,12 @@
 import jwt from 'jsonwebtoken'
+import { v4 as uuidv4 } from 'uuid'
 
 export const generateAuthResponse = (user) => {
   const secretKey = process.env.JWT_SECRET
+
+  if (!secretKey) {
+    throw new Error('JWT_SECRET is not defined in environment variables')
+  }
 
   const userInfo = {
     id: user.user_id,
@@ -9,7 +14,9 @@ export const generateAuthResponse = (user) => {
     last_name: user.last_name,
   }
 
-  const token = jwt.sign({ userId: user.user_id }, secretKey, {
+  const jti = uuidv4()
+
+  const token = jwt.sign({ userId: user.user_id, jti: jti }, secretKey, {
     expiresIn: '1h',
   })
 
