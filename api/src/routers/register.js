3daf4f35d6821dce11295fd71100ffb0945f1262
Natalia Lapina <lapinann08@gmail.com>
2024-11-28 18:57:27 +0100

    feat: apply errorDto to register function

--- api/src/routers/register.js
+++ api/src/routers/register.js
@@ -2,6 +2,7 @@ import express from 'express'
 import knex from '../database_client.js'
 import bcrypt from 'bcrypt'
 import { generateAuthResponse } from '../utils/auth.js'
+import { buildErrorDto } from '../dtos/errorDto.js'
 
 const register = express.Router()
 
@@ -10,13 +11,13 @@ register.post('/', async (req, res) => {
   const { email, password, first_name, last_name } = req.body
   if (!email || !password || !first_name || !last_name) {
     console.error('Some fields from the frontend are invalid')
-    return res.status(400).json({ message: 'All fields are required' })
+    return res.status(400).json(buildErrorDto('All fields are required'))
   }
   try {
     // Checking if the username already exists
     const existedUser = await knex('user').where({ email }).first()
     if (existedUser) {
-      return res.status(400).json({ message: 'Email already in use' })
+      return res.status(400).json(buildErrorDto('Email already in use'))
     }
     // Hashing the password before save in dB
     const saltRounds = 10
@@ -36,8 +37,12 @@ register.post('/', async (req, res) => {
       ...authResponse,
     })
   } catch (error) {
-    res.status(500).json({ error: `Registration error: ${error.message}` })
-    console.log(error)
+    console.error('Registration error:', error)
+    res.status(500).json(
+      buildErrorDto('Error registering user', {
+        cause: error.message,
+      })
+    )
   }
 })
 
