48b8f66f2338331a1394818981dd776d2761f444
Natalia Lapina <lapinann08@gmail.com>
2024-11-28 19:13:34 +0100

    feat: apply errorDto to refreshToken function

--- api/src/routers/refreshToken.js
+++ api/src/routers/refreshToken.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken'
 import knex from '../database_client.js'
 import { generateAuthResponse } from '../utils/auth.js'
 import { invalidateAuthToken, cleanupInvalidatedTokens } from '../utils/auth.js'
+import { buildErrorDto } from '../dtos/errorDto.js'
 import config from '../config.js'
 
 const SECRET_KEY = config.JWT_SECRET
@@ -13,7 +14,7 @@ refreshToken.post('/', async (req, res) => {
   const token = req.header('Authorization')?.split(' ')[1]
 
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' })
+    return res.status(401).json(buildErrorDto('No token provided'))
   }
 
   let decoded
@@ -21,16 +22,16 @@ refreshToken.post('/', async (req, res) => {
     decoded = jwt.verify(token, SECRET_KEY)
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
-      return res.status(400).json({ message: 'Token expired' })
+      return res.status(400).json(buildErrorDto('Token expired'))
     }
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json(buildErrorDto('Invalid token'))
   }
 
   try {
     const user = await knex('user').where({ user_id: decoded.userId }).first()
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' })
+      return res.status(404).json(buildErrorDto('User not found'))
     }
 
     // Invalidate old token, clean up DB and generate new one
@@ -40,7 +41,11 @@ refreshToken.post('/', async (req, res) => {
     res.status(200).json(authResponse)
   } catch (error) {
     console.error('Database or token operation error:', error)
-    return res.status(500).json({ message: 'Internal server error' })
+    return res.status(500).json(
+      buildErrorDto('Error refreshing token', {
+        cause: error.message,
+      })
+    )
   }
 })
 
