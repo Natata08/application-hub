63e7f1169e57db6f67f682f636d820927181a044
FurkannOzbek <furkannozbek@yandex.com>
2024-11-30 00:45:17 +0100

    fix:minor name fix

--- api/src/controllers/interviewController.js
+++ api/src/controllers/interviewController.js
@@ -2,71 +2,68 @@ import knex from '../database_client.js'
 import { buildInterviewsDto } from '../dtos/interviewsDto.js'
 import { buildInterviewDto } from '../dtos/interviewsDto.js'
 import { buildErrorDto } from '../dtos/errorDto.js'
+import { checkApplicationExistByUserId } from '../utils/checkApplicationExist.js'
 
 export const getUserInterviews = async (req, res) => {
+  const user_id = req.userInfo.userId
+  const application_id = parseInt(req.params.id)
   try {
-    const user_id = req.userInfo.userId
-    const application_id = parseInt(req.params.id)
     if (!application_id || isNaN(application_id)) {
       return res.status(400).json(buildErrorDto('Invalid application ID'))
     }
-    try {
-      // User can only see its own interviews
-      const interviews = await knex('interview')
-        .join(
-          'application',
-          'interview.application_id',
-          '=',
-          'application.application_id'
-        )
-        .where('interview.application_id', application_id)
-        .andWhere('application.user_id', user_id)
-
-      if (!interviews || interviews.length === 0) {
-        return res.status(204).json({ message: 'No interviews found' })
-      }
-
-      return res.json(buildInterviewsDto(interviews))
-    } catch (error) {
-      console.error('Database error:', error)
-      return res.status(500).json(buildErrorDto('Database error'))
-    }
   } catch (error) {
-    console.error('Problem with id', error)
+    console.error('Problem with ID or request data:', error)
     return res.status(500).json(buildErrorDto('Internal server error'))
+  }
+
+  try {
+    const interviews = await knex('interview')
+      .join(
+        'application',
+        'interview.application_id',
+        '=',
+        'application.application_id'
+      )
+      .where('interview.application_id', application_id)
+      .andWhere('application.user_id', user_id)
+    if (interviews.length === 0) {
+      return res.status(200).json({
+        message: 'No interviews found for the specified application and user ',
+      })
+    }
+
+    return res.json(buildInterviewsDto(interviews))
+  } catch (error) {
+    console.error('Database error:', error)
+    return res.status(500).json(buildErrorDto('Database error'))
   }
 }
 
 export const postUserInterview = async (req, res) => {
   try {
     const user_id = req.userInfo.userId
-    const applicationId = parseInt(req.params.id)
+    const application_id = parseInt(req.params.id)
     const { type, scheduled_at, location, is_virtual } = req.body
-    if (!applicationId || isNaN(applicationId)) {
+    if (!application_id || isNaN(application_id)) {
       return res.status(400).json(buildErrorDto('Invalid application ID'))
     }
     if (!scheduled_at && !is_virtual) {
       return res.status(400).json(buildErrorDto('Interview details required'))
     }
     try {
-      // Check if the application exists and the user_id matches
-      const application = await knex('application')
-        .where('application_id', applicationId)
-        .select('user_id')
-        .first()
-
-      if (!application || application.user_id !== user_id) {
+      const isApplicationExist = await checkApplicationExistByUserId(
+        application_id,
+        user_id
+      )
+      if (!isApplicationExist) {
         return res
           .status(401)
-          .json(
-            buildErrorDto(
-              'Unauthorized access: User does not own this application'
-            )
-          )
+          .json(buildErrorDto('Application not found with this user'))
       }
+
       const [interview] = await knex('interview')
         .insert({
-          application_id: applicationId,
+          application_id,
           type,
           scheduled_at,
           location,
@@ -102,23 +99,14 @@ export const deleteUserInterview = async (req, res) => {
     return res.status(400).json(buildErrorDto('Invalid application ID'))
   }
   try {
-    const application = await knex('application')
-      .where('application_id', application_id)
-      .select('user_id')
-      .first()
-
-    if (!application) {
-      return res.status(404).json(buildErrorDto('Application not found'))
-    }
-
-    if (application.user_id !== user_id) {
+    const isApplicationExist = await checkApplicationExistByUserId(
+      application_id,
+      user_id
+    )
+    if (!isApplicationExist) {
       return res
         .status(401)
-        .json(
-          buildErrorDto(
-            'Unauthorized access: User does not own this application'
-          )
-        )
+        .json(buildErrorDto('Application not found with this user'))
     }
     const interviewToDelete = await knex('interview')
       .where('application_id', application_id)
@@ -137,35 +125,27 @@ export const deleteUserInterview = async (req, res) => {
 
 export const patchUserInterview = async (req, res) => {
   const user_id = req.userInfo.userId
-  const applicationId = parseInt(req.params.id)
+  const application_id = parseInt(req.params.id)
   const { interview_id, type, scheduled_at, location, is_virtual } = req.body
-  if (!applicationId || isNaN(applicationId)) {
+  if (!application_id || isNaN(application_id)) {
     return res.status(400).json(buildErrorDto('Invalid application ID'))
   }
   if (!type && !scheduled_at) {
     return res.status(400).json(buildErrorDto('Interview details required'))
   }
   try {
-    const application = await knex('application')
-      .where('application_id', applicationId)
-      .select('user_id')
-      .first()
-
-    if (!application) {
-      return res.status(404).json(buildErrorDto('Application not found'))
-    }
-
-    if (application.user_id !== user_id) {
+    const isApplicationExist = await checkApplicationExistByUserId(
+      application_id,
+      user_id
+    )
+    if (!isApplicationExist) {
       return res
         .status(401)
-        .json(
-          buildErrorDto(
-            'Unauthorized access: User does not own this application'
-          )
-        )
+        .json(buildErrorDto('Application not found with this user'))
     }
     const updatedRows = await knex('interview')
       .where('interview_id', interview_id)
+      .andWhere('application_id', application_id)
       .update({
         type,
         scheduled_at,
@@ -182,6 +162,12 @@ export const patchUserInterview = async (req, res) => {
         'created_at',
         'updated_at',
       ])
+
+    if (updatedRows.length === 0) {
+      return res
+        .status(404)
+        .json(buildErrorDto('Interview not found or update failed'))
+    }
 
     res.status(200).json(updatedRows)
   } catch (error) {
